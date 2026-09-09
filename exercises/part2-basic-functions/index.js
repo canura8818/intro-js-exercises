f63c1574e5431267f9078cf35569c18077b5379a
Tatar Anurakwongsri <chanon.anurakwongsri@gmail.com>
2026-09-09 14:14:19 -0400

    exercises

--- exercises/part2-basic-functions/index.js
+++ exercises/part2-basic-functions/index.js
@@ -13,16 +13,18 @@ in years. (Let's just assume this person was born January 1 at 12:00 AM)
 Example: "age(2000) should return 26"
 ===================== */
 
-let age = (birthYear) => {};
+let age = (birthYear) => 2026 - birthYear;
 
 console.log('age success:', age(1971) === 55);
+
+
 
 /* =====================
 Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = () => {};
+let plusOne = (number) => number + 1;
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -31,7 +33,7 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = () => {};
+let timesThree = (number) => number * 3;
 
 console.log('timesThree success:', timesThree(33) === 99);
 
@@ -40,7 +42,7 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = () => {};
+let add = (num1, num2) => num1 + num2;
 
 console.log('add success:', add(4, 6) === 10);
 
@@ -49,7 +51,7 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = () => {};
+let multiply = (num1, num2) => num1 * num2;
 
 console.log('multiply success:', multiply(4, 6) === 24);
 
@@ -57,7 +59,7 @@ console.log('multiply success:', multiply(4, 6) === 24);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-let isEven = () => {};
+let isEven = (number) => (number % 2) == 0;
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -65,7 +67,7 @@ console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 Instructions: Write a function that returns true if a number is odd.
 ===================== */
 
-let isOdd = () => {};
+let isOdd = (number) => (number % 2) == 1;
 
 console.log('isOdd success:', isOdd(4) === false && isOdd(5) === true);
 
@@ -74,7 +76,7 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = () => {};
+let valueAtIndex = (array, index) => array[index];
 
 console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
 
@@ -83,7 +85,7 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({name: 'Nathan', age: 27}, 'name') should return 'Nathan'"
 ===================== */
 
-let valueAtKey = () => {};
+let valueAtKey = (map, key) => map[key];
 
 console.log('valueAtKey success:', valueAtKey({ 'foo': 'bar' }, 'foo') === 'bar');
 
@@ -92,7 +94,7 @@ Instructions: "Write a function that returns the index of a value in an array"
 Example: "indexForValue(['Mercury', 'Venus', 'Earth', 'Mars'], 'Earth') should return 2"
 ===================== */
 
-let indexForValue = () => {};
+let indexForValue = (array, word) => array.indexOf(word);
 
 console.log('indexForValue success:', indexForValue([9, 8, 7, 6, 5], 7) === 2);
 
@@ -101,6 +103,7 @@ Instructions: "Write a function that returns the key of a value in an object"
 Example: "keyForValue({name: 'Nathan', age: 27}, 'Nathan') should return 'name'"
 ===================== */
 
-let keyForValue = () => {};
+let keyForValue = (map, value) => {return Object.keys(map).find((key) => map[key] === value);};
 
 console.log('keyForValue success:', keyForValue({ 'foo': 'bar', 'baz': 'qux' }, 'bar') === 'foo');
+
